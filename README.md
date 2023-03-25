8d2b247577e68d0630ad54f7131ef839041b6e7c
gato365 <immanuelwilliams@gato365.com>
2023-03-25 15:06:57 -0700

    This is what ChatGPT 4 provide me

--- README.md
+++ README.md
@@ -1,3 +1,51 @@
 # project-1-enhancing-my-memory
 
 Purpose: I am in the process of becoming better developer, computer scientist, and data scientist. I am using this project to my ability to organize my thoughts and ideas. I am also using this project to practice my skills in writing and presenting my ideas to ChatGPT and other things.
+
+
+## Future Design & Functionality Ideas:
+ 
+
+### My Ideas based on what I want to enhance:
+
+ - Display time and date
+ - Use this for music chords and e numbers
+
+ ### ChatGPT's Questions:
+
+- Do you have any specific requirements for the database that stores the memorized content?
+- Will users need to create an account or log in to use the website?
+- How will you ensure the security of user data and prevent unauthorized access to it?
+- Do you have any additional features or functionality that you would like to include in the project?
+- What is the target audience for this website?
+- Do you have any design or branding guidelines that I should be aware of?
+- What is the timeline for this project? Do you have any specific deadlines?
+- Are there any other details that you would like to share about the project?
+
+
+## Original Design & Function of App 
+
+Develop this app please
+## Name of the project: Memorize
+
+**Description of the project:** Memorize is a website that allows you to memorize anything you want. 
+0) A title name: Testing Eman's memory, a textbox, and save button are the only things on the screen
+1) The *User* **enters** the string that needs to be memorized in textbox
+2) The *User* **clicks** save button, when saved button is clicked:
+    - The textbox and save button disappear, entered text is stored in local storage
+    - Two new buttons appear that say: "start" and "stop"
+    - A timer is at 0:00
+3) Once the *User* **clicks** the "start" button:
+    - The user enters the what they remember in textbox
+    - The timer begins
+    - The website is calculating the % correct as the *User* is typing
+4) The *User* **clicks** the "stop" button when the *User* is complete then a stats bar shows up that displays:
+    - % correct
+    - Time to complete
+Design 
+    - The style is dark futuristic
+    - Everything is center on this page
+
+
+What programming languages and technologies are you using for this project? :HTML, CSS, and JavaScript
+This version is only for me
---
+++ app.js
@@ -0,0 +1,65 @@
+const memoryText = document.getElementById('memory-text');
+const saveBtn = document.getElementById('save-btn');
+const startBtn = document.getElementById('start-btn');
+const stopBtn = document.getElementById('stop-btn');
+const userInput = document.getElementById('user-input');
+const timer = document.getElementById('timer');
+const inputSection = document.querySelector('.input-section');
+const timerSection = document.querySelector('.timer-section');
+const statsSection = document.querySelector('.stats-section');
+const percentCorrect = document.getElementById('percent-correct');
+const timeToComplete = document.getElementById('time-to-complete');
+
+let startTime = null;
+let timerInterval = null;
+
+saveBtn.addEventListener('click', () => {
+  localStorage.setItem('memorizeText', memoryText.value);
+  inputSection.classList.add('hidden');
+  timerSection.classList.remove('hidden');
+});
+
+startBtn.addEventListener('click', () => {
+  startTime = new Date();
+  timerInterval = setInterval(updateTimer, 1000);
+  startBtn.classList.add('hidden');
+  stopBtn.classList.remove('hidden');
+  userInput.classList.remove('hidden');
+  userInput.focus();
+  userInput.addEventListener('input', calculatePercentage);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerInterval);
+  const totalTime = Math.floor((new Date() - startTime) / 1000);
+  const percentage = calculatePercentage();
+  percentCorrect.textContent = `Percentage correct: ${percentage}%`;
+  timeToComplete.textContent = `Time to complete: ${totalTime} seconds`;
+  statsSection.classList.remove('hidden');
+  stopBtn.classList.add('hidden');
+  userInput.classList.add('hidden');
+  userInput.removeEventListener('input', calculatePercentage);
+});
+
+function updateTimer() {
+  const currentTime = new Date();
+  const elapsedTime = Math.floor((currentTime - startTime) / 1000);
+  const minutes = Math.floor(elapsedTime / 60);
+  const seconds = elapsedTime % 60;
+  timer.textContent = `${minutes}:${seconds.toString().padStart(2, '0')}`;
+}
+
+function calculatePercentage() {
+  const originalText = localStorage.getItem('memorizeText');
+  const enteredText = userInput.value;
+  let correctChars = 0;
+
+  for (let i = 0; i < enteredText.length && i < originalText.length; i++) {
+    if (enteredText[i] === originalText[i]) {
+      correctChars++;
+    }
+  }
+
+  const percentage = Math.floor((correctChars / originalText.length) * 100);
+  return percentage;
+}
