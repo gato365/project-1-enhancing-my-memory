8f44a3b05b9869274da383ee9f6551f23278765f
gato365 <immanuelwilliams@gato365.com>
2023-03-26 10:11:47 -0700

    Change location of directories to make v2

---
+++ legacy/v1/README.md
@@ -0,0 +1,107 @@
+# project-1-enhancing-my-memory
+
+Purpose: I am in the process of becoming better developer, computer scientist, and data scientist. I am using this project to my ability to organize my thoughts and ideas. I am also using this project to practice my skills in writing and presenting my ideas to ChatGPT and other things.
+
+
+## Future Design & Functionality Ideas:
+ 
+
+### My Ideas based on what I want to enhance:
+
+#### Current Version:
+
+* [X] Prevent auto refresh
+* [X] If string is too long take 10% off if 0 keep at 0
+* [X] Display time and date
+* [X] Play again button on last page
+* [X] Do not store text in web browser
+* [X] Why-No  asynchronusfunctionality JavaScript
+* [X] Make colors more appealing
+* [X] Start the game issue, things needed to be hidden again
+* [X] Display original text and entered text
+* [X] Save scores in local storage
+* [X] Show Text as it is being entered
+
+ #### In a future version:
+
+##### Level 1:
+
+* [ ] Use this for music chords and e numbers Process
+* [ ] Filter word numbers and letters and display it on the screen
+* [ ] Display selected groups and sets
+
+##### Level 2:
+
+* [ ] Give the ability to set a time goal
+* [ ] If goal is not met, take points off
+* [ ] Create a score board
+* [ ] Save scores in a database
+* [ ] Perform unit testing
+* [ ] Create a visualization of past scores
+
+ ### ChatGPT's Questions:
+
+- Do you have any specific requirements for the database that stores the memorized content?
+- Will users need to create an account or log in to use the website?
+- How will you ensure the security of user data and prevent unauthorized access to it?
+- Do you have any additional features or functionality that you would like to include in the project?
+- What is the target audience for this website?
+- Do you have any design or branding guidelines that I should be aware of?
+- What is the timeline for this project? Do you have any specific deadlines?
+- Are there any other details that you would like to share about the project?
+
+
+## Original Design & Function of App 
+
+Develop this app please
+## Name of the project: Memorize
+
+**Description of the project:** Memorize is a website that allows you to memorize anything you want. 
+0) A title name: Testing Eman's memory, a textbox, and save button are the only things on the screen
+1) The *User* **enters** the string that needs to be memorized in textbox
+2) The *User* **clicks** save button, when saved button is clicked:
+    - The textbox and save button disappear, entered text is stored in local storage
+    - Two new buttons appear that say: "start" and "stop"
+    - A timer is at 0:00
+3) Once the *User* **clicks** the "start" button:
+    - The user enters the what they remember in textbox
+    - The timer begins
+    - The website is calculating the % correct as the *User* is typing
+4) The *User* **clicks** the "stop" button when the *User* is complete then a stats bar shows up that displays:
+    - % correct
+    - Time to complete
+Design 
+    - The style is dark futuristic
+    - Everything is center on this page
+
+
+What programming languages and technologies are you using for this project? :HTML, CSS, and JavaScript
+This version is only for me
+
+
+
+## Image of Image of Original Design & Function of App
+
+### Origial Idea Written
+
+<img src = "images/plan-0.jpg" width="35%" height="65%"> 
+
+### First Page
+
+<img src = "images/image_0_version_0.png" width="35%" height="35%"> 
+
+### Enter Original Text & Click Save Button
+
+<img src = "images/image_1_version_0.png" width="35%" height="35%">
+
+###  Click Start Button
+
+<img src = "images/image_2_version_0.png" width="35%" height="35%">
+
+### Enter Text & Click Stop Button
+
+<img src = "images/image_3_version_0.png" width="35%" height="35%">
+
+### Stats Bar
+
+<img src = "images/image_4_version_0.png" width="35%" height="35%">
---
+++ legacy/v1/app.js
@@ -0,0 +1,211 @@
+function updateTimer() {
+
+    const currentTime = new Date();
+    const elapsedTime = Math.floor((currentTime - startTime) / 1000);
+    const minutes = Math.floor(elapsedTime / 60);
+    const seconds = elapsedTime % 60;
+    timer.textContent = `${minutes}:${seconds.toString().padStart(2, '0')}`;
+}
+function calculatePercentage() {
+    const originalText = localStorage.getItem('memorizeText');
+    const enteredText = userInput.value;
+    let correctChars = 0;
+
+    for (let i = 0; i < enteredText.length && i < originalText.length; i++) {
+        if (enteredText[i] === originalText[i]) {
+            correctChars++;
+        }
+    }
+
+    let percentage = Math.floor((correctChars / originalText.length) * 100);
+
+    // If the entered text is longer than the original text, take 10% off the percentage.
+    if (enteredText.length > originalText.length) {
+        percentage = percentage - 0.05 * (enteredText.length - originalText.length);
+        // percentage = Math.floor(percentage); // Round down to the nearest integer after taking 10% off.
+    }
+
+    // Ensure the percentage stays within the range of 0 to 100.
+    if (percentage < 0) {
+        percentage = 0;
+    } else if (percentage > 100) {
+        percentage = 100;
+    }
+
+
+
+    // Return the percentage
+    return percentage;
+}
+function updateTimeAndDate() {
+    const currentTimeDate = new Date();
+    const formattedTimeDate = currentTimeDate.toLocaleString();
+    document.getElementById('time-date').textContent = formattedTimeDate;
+}
+
+let e_data;
+async function loadJSON(url) {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      e_data = await response.json();
+    console.log(e_data);
+    } catch (error) {
+      console.error('Error fetching JSON:', error);
+    }
+  }
+
+  loadJSON('eman.json');
+//   console.log(e_data)
+
+const memoryText = document.getElementById('memory-text');
+const saveBtn = document.getElementById('save-btn');
+const startBtn = document.getElementById('start-btn');
+const stopBtn = document.getElementById('stop-btn');
+const userInput = document.getElementById('user-input');
+const timer = document.getElementById('timer');
+const inputSection = document.querySelector('.input-section');
+const timerSection = document.querySelector('.timer-section');
+const statsSection = document.querySelector('.stats-section');
+const percentCorrect = document.getElementById('percent-correct');
+const timeToComplete = document.getElementById('time-to-complete');
+// Add this line to the top of your JavaScript file to get the "Play Again" button element
+const playAgainBtn = document.getElementById('play-again-btn');
+// Add this line to the top of your JavaScript file to get the "Try Again" button element
+const tryAgainBtn = document.getElementById('try-again-btn');
+// Add these lines at the top of your JavaScript file to get the new elements
+const showTextsBtn = document.getElementById('show-texts-btn');
+const textsContainer = document.getElementById('texts-container');
+const originalTextDisplay = document.getElementById('original-text');
+const enteredTextDisplay = document.getElementById('entered-text');
+const currentEnteredText = document.getElementById('current-entered');
+// number and letter 
+const numbers = document.getElementById('numbers');
+const letters = document.getElementById('letters');
+
+
+let startTime = null;
+let timerInterval = null;
+
+saveBtn.addEventListener('click', () => {
+    localStorage.setItem('memorizeText', memoryText.value);
+    inputSection.classList.add('hidden');
+    timerSection.classList.remove('hidden');
+});
+
+
+
+
+
+
+
+
+startBtn.addEventListener('click', () => {
+    startTime = new Date();
+    timerInterval = setInterval(updateTimer, 1000);
+    startBtn.classList.add('hidden');
+    stopBtn.classList.remove('hidden');
+    userInput.classList.remove('hidden');
+    userInput.focus();
+    userInput.addEventListener('input', calculatePercentage);
+  
+    // Hide the text containers when the user starts typing
+    textsContainer.classList.add('hidden');
+    originalTextDisplay.classList.add('hidden');
+    enteredTextDisplay.classList.add('hidden');
+  
+    // Add event listener to update currentEnteredText as the user types
+    userInput.addEventListener('input', () => {
+      currentEnteredText.classList.remove('hidden');
+      currentEnteredText.textContent = `Current Entered Text: ${userInput.value}`;
+    });
+  });
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(timerInterval);
+    const totalTime = Math.floor((new Date() - startTime) / 1000);
+    const percentage = calculatePercentage();
+    percentCorrect.textContent = `Percentage correct: ${percentage}%`;
+    timeToComplete.textContent = `Time to complete: ${totalTime} seconds`;
+    statsSection.classList.remove('hidden');
+    stopBtn.classList.add('hidden');
+    userInput.classList.add('hidden');
+    userInput.removeEventListener('input', calculatePercentage);
+    currentEnteredText.textContent = '';
+    // Show the "Play Again" button
+    playAgainBtn.classList.remove('hidden');
+    // Show the "Try Again" button
+    tryAgainBtn.classList.remove('hidden');
+    // Show the "Show Original and Entered Texts" button
+    showTextsBtn.classList.remove('hidden');
+
+
+
+    // Save the percentage to localStorage
+    const currentAttemptData = {
+        percentage: percentage,
+        time: totalTime,
+        memorizeText: localStorage.getItem('memorizeText'),
+        enteredText: userInput.value
+    };
+
+
+    localStorage.setItem('currentAttemptData', JSON.stringify(currentAttemptData));
+
+});
+
+
+// Add this event listener for the "Play Again" button
+playAgainBtn.addEventListener('click', () => {
+    // Reset the user input, timer, and stats
+    userInput.value = '';
+    timer.textContent = '0:00';
+    percentCorrect.textContent = '';
+    timeToComplete.textContent = '';
+    currentEnteredText.textContent = '';
+
+    // Hide the stats section and "Play Again" button, show the input section
+    statsSection.classList.add('hidden');
+    playAgainBtn.classList.add('hidden');
+    tryAgainBtn.classList.add('hidden');
+    inputSection.classList.remove('hidden');
+    timerSection.classList.add('hidden');
+    memoryText.value = '';
+    startBtn.classList.remove('hidden');
+});
+
+// Add this event listener for the "Try Again" button
+tryAgainBtn.addEventListener('click', () => {
+    // Reset the user input and timer
+    userInput.value = '';
+    timer.textContent = '0:00';
+    currentEnteredText.textContent = '';
+
+    // Hide the stats section and "Try Again" button, show the timer section and "start" button
+    statsSection.classList.add('hidden');
+    tryAgainBtn.classList.add('hidden');
+    timerSection.classList.remove('hidden');
+    startBtn.classList.remove('hidden');
+    stopBtn.classList.add('hidden');
+});
+
+
+// Add this event listener for the "Show Original and Entered Texts" button
+showTextsBtn.addEventListener('click', () => {
+    const originalText = localStorage.getItem('memorizeText');
+    const enteredText = userInput.value;
+    
+
+    originalTextDisplay.textContent = `Original Text: ${originalText}`;
+    enteredTextDisplay.textContent = `Entered Text: ${enteredText}`;
+
+    textsContainer.classList.remove('hidden');
+    originalTextDisplay.classList.remove('hidden');
+    enteredTextDisplay.classList.remove('hidden');
+});
+
+
+updateTimeAndDate();
+
