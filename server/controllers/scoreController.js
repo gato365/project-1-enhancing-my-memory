3c6e6142e44ca20aea67c6bad744c67c0052fd8a
gato365 <immanuelwilliams@gato365.com>
2023-03-27 17:11:23 -0700

    Modified controller so that proper information

--- server/controllers/scoreController.js
+++ server/controllers/scoreController.js
@@ -1,20 +1,42 @@
 const { Score } = require('../models');
 
+module.exports = {
+    // POST /scores
+    async createScore(req, res) {
+        const { date, percentage, time, enteredText, selectedLetters, selectedNumbers, currentLine } = req.body;
 
-odule.exports = {
-    // GET /scores
-    createScore(req, res) {
-        Score.create(req.body)
-            .then(score => res.json(score))
-            .catch(err => res.status(422).json(err));
+        // You can add input validation here if necessary
+
+        try {
+            const score = new Score({ date, percentage, time, enteredText, selectedLetters, selectedNumbers, currentLine });
+            await score.save();
+            res.json(score);
+        } catch (err) {
+            res.status(422).json(err);
+        }
     },
 
-    // POST /scores
+    // GET /scores
     getScores(req, res) {
         Score.find(req.query)
-
             .then(scores => res.json(scores))
             .catch(err => res.status(422).json(err));
     },
-}
 
+    // GET /scores/:id
+    async getScore(req, res) {
+        const { id } = req.params;
+
+        // You can add input validation here if necessary
+
+        try {
+            const score = await Score.findById(id);
+            if (!score) {
+                return res.status(404).json({ error: 'Score not found.' });
+            }
+            res.json(score);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+}
