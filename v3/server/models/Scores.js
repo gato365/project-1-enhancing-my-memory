a55e4a22fb1116225b72810227fe129d083f23f5
gato365 <immanuelwilliams@gato365.com>
2023-07-02 13:51:45 -0700

    made some changes

---
+++ v3/server/models/Scores.js
@@ -0,0 +1,52 @@
+const { Schema, Types, model } = require('mongoose');
+
+
+// Save date, percentage,time, entered text selected Letters, selectedNumbers, currentLine
+const scoresSchema = new Schema(
+    {
+        date: {
+            type: Date,
+            default: Date.now,
+        },
+        percentage: {
+            type: Number,
+            required: true,
+        },
+        time: {
+            type: Number,
+            required: true,
+        },
+        enteredText: {
+            type: String,
+            required: true,
+        },
+        selectedLetters: {
+            type: String,
+            required: true,
+        },
+        selectedNumbers: {
+            type: String,
+            required: true,
+        },
+        currentLine: {
+            type: String,
+            required: true,
+        },
+        // This is the reference to the user that created this score
+        // user: { type: Types.ObjectId, ref: 'User' },
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+    }
+);
+
+const Scores = model('Scores', scoresSchema);
+
+module.exports = Scores;
+
+
+
+
+
---
+++ v3/steps-of-state.md
@@ -0,0 +1,47 @@
+Create a list of programming tasks that I would do to complete these tasks
+State 0 
+Action: Time Begins 
+Set: 1    
+Input Box: 
+Time: 0:00 
+Past Inputs: 
+Save Btn:
+
+
+State 1 
+Action: Entry Data 
+Set: 1
+Input Box: 123 
+Time: 0.10 
+Past Inputs: 
+Save Btn
+
+State 2 
+Action: Click save by User 
+Set: 1   
+Input Box: 123 
+Time: 0.10 
+Past Inputs: 
+Save Btn:
+
+
+
+State 3 
+Action: Information is store past Inputs 
+Set: 1 
+Input Box: 123 
+Time: 0:10 
+Past Inputs: Set 1 Str-123 Time-0:10 
+Save Btn:
+
+
+
+State 4: 
+Action: Information is erased & new set is formulated,
+Set: 2
+Input Box: 
+Time: 0:11 (Time is continued)
+Past Inputs: Set 1 Str-123 Time-0:10 
+Save Btn:
+
+
